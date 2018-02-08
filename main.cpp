d864947bf8ec0351da1dd37d4f5a0b3f8c7ed94b
yreyes1 <yreyes1@example.com>
2018-02-08 11:36:49 -0800

    Upper, Lower, Symbol and Number

--- main.cpp
+++ main.cpp
@@ -1,24 +1,35 @@
-//Author:
+//Author:Yaneth Reyes Hinojosa
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-
+  
+  char letters;
+  
   cout<<"What character do you want to know about?\n";
+  cin>>letters;
+  
+  if(letters >= 'A' && letters <= 'Z') 
 
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  {
+  cout<< letters<<" is an upper case letter!\n";
+  }
 
+  else if(letters >='a' && letters <= 'z')
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
+  {
+  cout<< letters <<" is a lower case letter!\n";
+  }
+  else
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+  {
+  cout<< letters <<"?! Pssh. What are you talking about?\n";
+  }
   //no matter what they enter...
   cout<<"The ASCII value is: ";
-
+  cout<< (int) letters<< "\n";
   return 0;
 }
